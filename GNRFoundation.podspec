aec375fe793662f12e0ce3132f0bfbf2882e050e
Luca <ly918@qq.com>
2018-05-23 18:13:51 +0800

    增加了header

--- GNRFoundation.podspec
+++ GNRFoundation.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'GNRFoundation'
-  s.version          = '0.2.1'
+  s.version          = '0.2.2'
   s.summary          = '好东西都在这里了.'
 
 # This description is used to generate tags and improve search results.
---
+++ GNRFoundation/Classes/GNRFoundationHeader.h
@@ -0,0 +1,20 @@
+//
+//  GNRFoundationHeader.h
+//  GNRFoundation
+//
+//  Created by Luca on 2018/5/23.
+//
+
+#ifndef GNRFoundationHeader_h
+#define GNRFoundationHeader_h
+
+#import <CALayer+Factory.h>
+#import <NSString+FirstLetter.h>
+#import <UIBarButtonItem+Factory.h>
+#import <UIColor+Hex.h>
+#import <UIImage+Fix.h>
+#import <UIView+Factory.h>
+#import <UIView+GNRSafeArea.h>
+#import <NSObject+TKObject.h>
+
+#endif /* GNRFoundationHeader_h */
